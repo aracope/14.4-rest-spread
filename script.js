1dc02278bdb24a0454eef4a845c537d66c0356a2
aracope <156398622+aracope@users.noreply.github.com>
2025-01-27 15:30:07 -0800

    Three out of ten functions written.

--- script.js
+++ script.js
@@ -6,15 +6,20 @@
 //     });
 //   }
 
+const filterOutOdds = (...arguments) => arguments.filter(num % 2 === 0);
+
 //   Write a function called findMin that accepts a variable number of arguments and returns the smallest argument.
 // Make sure to do this using the rest and spread operator.
 // findMin(1,4,12,-3) // -3
 // findMin(1,-1) // -1
 // findMin(3,1) // 1
 
+const findMin = (...arguments) => Math.min(...arguments);
+
 // Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
 // mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
 
+const mergeObjects = (object1, object2) => ({...object1, ...object2});
 
 // Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
 // doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
